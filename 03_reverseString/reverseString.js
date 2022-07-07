0fe65dbb98e6e9828a7478b06f802ad36bb463fd
danjun01 <dhj190001@utdallas.edu>
2022-07-07 15:41:23 -0500

    updated progress

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str.length < 1) return '';
+    if (str.length == 1) return str;
+    else {
+        return str[str.length - 1] + reverseString(str.slice(0,-1));
+    }
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...filter) {
+    return newArr;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
